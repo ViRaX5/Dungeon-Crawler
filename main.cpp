9adb1b81cfeb61b4f77b8afc7bf99b2ccde2a7dc
ViRaX5 <126521627+ViRaX5@users.noreply.github.com>
2026-03-18 22:37:41 +0200

    Changed to correct input file

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 {
 
     Game game;
-    game.loadFromFile("nivInput.txt");
+    game.loadFromFile("Input.txt");
 
     // Run game logic based on loaded commands
     game.executeCommands();
